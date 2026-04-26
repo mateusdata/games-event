f3bc300e3028e3fcf9c45b2d3df2415b088eb64c
Mateus Santos <mateuspele2015@gmail.com>
2026-04-25 21:33:22 -0300

    feat: add OfflinePage component for offline user experience

---
+++ app/~offline/page.tsx
@@ -0,0 +1,21 @@
+export default function OfflinePage() {
+  return (
+    <div style={{
+      height: "100vh",
+      background: "#111",
+      color: "#0f0",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "monospace",
+      textAlign: "center",
+      padding: "20px"
+    }}>
+      <h1 style={{ fontSize: "3rem", marginBottom: "20px" }}>SEM INTERNET mateus</h1>
+      <p style={{ fontSize: "1.3rem", maxWidth: "500px" }}>
+       Mateus
+      </p>
+    </div>
+  );
+}
